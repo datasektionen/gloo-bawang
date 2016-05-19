9a356046cad1b641d124085c8af45ea4dec92102
Mauritz Zachrisson <mauritz.zachrisson@gmail.com>
2016-05-20 00:31:18 +0200

    Moved browserify to grunt

--- gruntfile.js
+++ gruntfile.js
@@ -15,12 +15,36 @@ module.exports = function (grunt) {
         watch: {
             files: ['static/css/**/*.less'],
             tasks: ['less']
+        },
+        browserify: {
+            dev: {
+                files: {
+                    'static/js/news.js': ['static/js/news.jsx'],
+                    'static/js/first.js': ['static/js/first.jsx', 'static/js/stickies.jsx']
+                },
+                options: {
+                    transform: [
+                        ["babelify", {"presets": ["es2015", "react"]}]
+                    ]
+                }
+            }
+        },
+        uglify: {
+            dev: {
+                files: {
+                    'static/js/first.min.js': 'static/js/first.js',
+                    'static/js/news.min.js': 'static/js/news.js'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['less', 'watch']);
+    grunt.registerTask('default', ['less', 'browserify', 'uglify']);
+    grunt.registerTask('dev', ['less', 'browserify', 'uglify', 'watch']);
 
 };
